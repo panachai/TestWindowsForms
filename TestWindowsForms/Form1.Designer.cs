b0c981b76470a531fbbc9ea028bbc150cc323034
panachai <panachai.ny@gmail.com>
2017-05-09 18:59:50 +0700

    Add project files.

---
+++ TestWindowsForms/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TestWindowsForms {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.cbMonth = new System.Windows.Forms.ComboBox();
+            this.tbYear = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.lbTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbMonth
+            // 
+            this.cbMonth.AccessibleName = "";
+            this.cbMonth.FormattingEnabled = true;
+            this.cbMonth.Items.AddRange(new object[] {
+            "มกราคม",
+            "กุมภาพันธ์",
+            "มีนาคม",
+            "เมษายน",
+            "พฤษภาคม",
+            "มิถุนายน",
+            "กรกฎาคม",
+            "สิงหาคม",
+            "กันยายน",
+            "ตุลาคม",
+            "พฤศจิกายน",
+            "ธันวาคม"});
+            this.cbMonth.Location = new System.Drawing.Point(13, 13);
+            this.cbMonth.Name = "cbMonth";
+            this.cbMonth.Size = new System.Drawing.Size(121, 21);
+            this.cbMonth.TabIndex = 0;
+            this.cbMonth.SelectedIndexChanged += new System.EventHandler(this.cbMonth_SelectedIndexChanged);
+            // 
+            // tbYear
+            // 
+            this.tbYear.Location = new System.Drawing.Point(141, 13);
+            this.tbYear.Name = "tbYear";
+            this.tbYear.Size = new System.Drawing.Size(100, 20);
+            this.tbYear.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.rtbResult.Location = new System.Drawing.Point(13, 41);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(310, 171);
+            this.rtbResult.TabIndex = 3;
+            this.rtbResult.Text = "";
+            this.rtbResult.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // lbTest
+            // 
+            this.lbTest.AutoSize = true;
+            this.lbTest.Location = new System.Drawing.Point(13, 219);
+            this.lbTest.Name = "lbTest";
+            this.lbTest.Size = new System.Drawing.Size(35, 13);
+            this.lbTest.TabIndex = 4;
+            this.lbTest.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 261);
+            this.Controls.Add(this.lbTest);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbYear);
+            this.Controls.Add(this.cbMonth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbMonth;
+        private System.Windows.Forms.TextBox tbYear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Label lbTest;
+    }
+}
+
---
+++ TestWindowsForms/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWindowsForms {
+    public partial class Form1 : Form {
+        MyCarlendar myCarlendar;
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            myCarlendar = new MyCarlendar();
+
+            //------------------- fixed form
+            // Define the border style of the form to a dialog box.
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            // Set the MaximizeBox to false to remove the maximize box.
+            this.MaximizeBox = false;
+            // Set the MinimizeBox to false to remove the minimize box.
+            this.MinimizeBox = false;
+            // Set the start position of the form to the center of the screen.
+            this.StartPosition = FormStartPosition.CenterScreen;
+            // Display the form as a modal dialog box. //this.ShowDialog();
+            //-------------------
+
+            //------------------- control combobox
+            cbMonth.SelectedIndex = cbMonth.Items.IndexOf("มกราคม");
+            cbMonth.DropDownStyle = ComboBoxStyle.DropDownList;
+            //-------------------
+            tbYear.Text = "2017";
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            rtbResult.Text = ("Mo  Tu  We  Th  Fr  Sa  Su\n" + myCarlendar.OnCheck(cbMonth.Text, Int32.Parse(tbYear.Text)));    //+String เข้าเอา
+            //System.Diagnostics.Debug.WriteLine("year : "+tbYear.Text);
+            // Int32.Parse(tbYear.Text)
+            //lbTest.Text = (string)cbMonth.SelectedItem;
+        }
+
+        private void cbMonth_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+    }
+
+
+    class MyCarlendar {
+
+        public String OnCheck(String month, int year) {
+            int result;
+
+            if (month.Equals("มกราคม")) {
+                result = 01;
+            }
+            else if (month.Equals("กุมภาพันธ์")) {
+                result = 02;
+            }
+            else if (month.Equals("มีนาคม")) {
+                result = 03;
+            }
+            else if (month.Equals("เมษายน")) {
+                result = 04;
+            }
+            else if (month.Equals("พฤษภาคม")) {
+                result = 05;
+            }
+            else if (month.Equals("มิถุนายน")) {
+                result = 06;
+            }
+            else if (month.Equals("กรกฎาคม")) {
+                result = 07;
+            }
+            else if (month.Equals("สิงหาคม")) {
+                result = 08;
+            }
+            else if (month.Equals("กันยายน")) {
+                result = 09;
+            }
+            else if (month.Equals("ตุลาคม")) {
+                result = 10;
+            }
+            else if (month.Equals("พฤศจิกายน")) {
+                result = 11;
+            }
+            else if (month.Equals("ธันวาคม")) {
+                result = 12;
+            }
+            else {
+                result = 0;
+            }
+
+            return OnGetDayByMonth(year, result);
+        }
+        public String OnGetDayByMonth(int year, int indexMonth) {
+
+            //เดือนหลัก
+            int lastMonth = DateTime.DaysInMonth(year, indexMonth);
+            int startWeek = (int)new DateTime(year, indexMonth, 1).DayOfWeek;
+
+            //เก็บค่า
+            string dayMonth = "";
+
+            //ก่อนเดือนหลัก
+            int lastMonthBeforeWeek = 0;
+            int startBeforeWeek;
+
+            int count = 1;
+            if (startWeek != 1) {   //ทำค่าก่อนหน้า ของเดือนนี้
+
+
+                if (indexMonth != 1) {
+                    //Console.WriteLine("index month : " + indexMonth);
+                    startBeforeWeek = (int)new DateTime(year, indexMonth - 1, 1).DayOfWeek;
+
+                    lastMonthBeforeWeek = DateTime.DaysInMonth(year, indexMonth - 1);
+                }
+                else {
+                    startBeforeWeek = (int)new DateTime(year - 1, 12, 1).DayOfWeek;
+
+                    lastMonthBeforeWeek = DateTime.DaysInMonth(year - 1, 12);
+                }
+
+                Console.WriteLine("StartBefore : " + startBeforeWeek);
+
+                int copyLMBW = lastMonthBeforeWeek;
+                int copySBFW = startBeforeWeek;
+
+                while (startBeforeWeek != 1) {
+                    dayMonth = "   " + lastMonthBeforeWeek + dayMonth;
+                    copyLMBW--;
+                    copySBFW--;
+                }
+
+                dayMonth += "   ";//เว้นก่อนลง ปฏิทินหลัก
+                Console.WriteLine("before DayMonth : " + dayMonth);
+            }
+
+            int checkfirstloop = 0;
+
+          
+
+            for (int i = 1; i <= lastMonth; i++) {
+                dayMonth += i + "   ";
+
+                if (checkfirstloop == 0) {
+                    //startBeforeWeek
+                   
+                    if (count == 7 - startBeforeWeek) {
+                        count = 1;
+                        dayMonth += "\n";
+                        checkfirstloop = 1;
+                    }
+                }
+                else {
+                    if (count == 7) {
+                        count = 1;
+                        dayMonth += "\n";
+                    }
+                }
+                count++;
+            }
+
+            //System.Diagnostics.Debug.WriteLine("test : " + startWeek);
+            //System.Diagnostics.Debug.WriteLine("test weekname : " + new DateTime(year, indexMonth, 1).DayOfWeek);
+
+            //System.Diagnostics.Debug.WriteLine(dayMonth);
+
+            return dayMonth;
+        }
+
+
+    }
+}
